b87304b5bf7709d8068973ba5e7db5c789b5b4b2
HS <77558413+crohasang@users.noreply.github.com>
2024-07-10 03:37:43 -0400

    Feat: Create login page and KaokaoLogin component (#6)
    * Chore: Bring kakaoLoginBtn image
    
    * Feat: Create login page and KakaoLogin component
    
    * Rename: Create folders in assets > image and move kakaoLoginBtn
    
    ---------
    
    Co-authored-by: Seohyun Jung <tjgus52213@gmail.com>

--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,14 @@ import { /*Outlet,*/ Route, Routes } from 'react-router-dom';
 import Loading from './components/Loading';
 import Layout from './layouts/Layout';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 const Router = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/loading" element={<Loading />} />
         {/* <Route path="/order" element={<Outlet />}>
           <Route index element={<Order />} />
