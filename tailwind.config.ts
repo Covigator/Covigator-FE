a8d1cd6a311c9f508f54a56da14ee4316625487a
hyunn522 <tjgus52213@gmail.com>
2024-07-14 17:18:03 +0900

    style: color 정의

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,35 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/containers/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  themes: {
+  theme: {
     extend: {
-      // [TODO] 디자인 시스템 확정 시 추가 예정
-      colors: {},
+      colors: {
+        // primary color
+        'primary-100': '#F9E8EE',
+        'primary-200': '#F7D8E0',
+        'primary-300': '#F5C6D1',
+        'primary-400': '#F299AF',
+        'primary-500': '#F174A3',
+
+        // sub color
+        'sub-100': '#B089BE',
+        'sub-200': '#E7EEFF',
+        'sub-300': '#606AB1',
+        'sub-400': '#2D387A',
+
+        // grayscale
+        wh: '#FFFFFF',
+        'bk-10': '#F8F7F7',
+        'bk-20': '#F4F2F0',
+        'bk-30': '#EBE9E7',
+        'bk-40': '#DBD9D7',
+        'bk-50': '#B7B6B4',
+        'bk-60': '#9E9E9E',
+        'bk-70': '#616161',
+        'bk-80': '#424242',
+        'bk-90': '#302E2D',
+        'bk-100': '#121212',
+      },
       fontWeight: {},
       fontFamily: {},
       typos: {},
