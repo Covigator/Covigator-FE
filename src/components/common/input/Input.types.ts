65f38bfe472b98a4ef6967b29002c11ce3af4e25
hyunn522 <tjgus52213@gmail.com>
2024-07-19 22:02:38 +0900

    feat: add inputSize type

--- src/components/common/input/Input.types.ts
+++ src/components/common/input/Input.types.ts
@@ -1,3 +1,7 @@
-// export type InputProps = {
-
-// }
+export const inputSize = {
+    sm: 'sm',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+} as const;
+export type InputSize = (typeof inputSize)[keyof typeof inputSize];
