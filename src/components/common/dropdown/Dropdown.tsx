f1299c6c0d3896bea49810daef6824cccc8c36e7
hyunn522 <tjgus52213@gmail.com>
2024-07-22 17:11:08 +0900

    feat: add dropdown btns and items

--- src/components/common/dropdown/Dropdown.tsx
+++ src/components/common/dropdown/Dropdown.tsx
@@ -1,5 +1,45 @@
-const DropDown = () => {
-  return <div>DropDown</div>;
+import { useRef, useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import { DropdownProps, DropdownSize } from './Dropdown.types';
+
+const styles: {
+  base: string;
+  sizes: Record<DropdownSize, string>;
+} = {
+  base: '',
+  sizes: {
+    sm: '',
+    lg: '',
+  },
+};
+
+const DropDown = ({ dropdownItems, size }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedItem, setSelectedItem] = useState<string>(dropdownItems[0]);
+
+  const handleSelect = (item: string) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <div className="w-full h-10">
+      <Dropdown className="w-full bg-bk-40">
+        {/* 드롭다운 버튼 */}
+        <Dropdown.Toggle>{selectedItem}</Dropdown.Toggle>
+        {/* 드롭다운 메뉴 */}
+        <Dropdown.Menu>
+          {dropdownItems.map((item: string) => {
+            return (
+              <Dropdown.Item key={item} onClick={(e) => handleSelect(item)}>
+                {item}
+              </Dropdown.Item>
+            );
+          })}
+        </Dropdown.Menu>
+      </Dropdown>
+    </div>
+  );
 };
 
 export default DropDown;
