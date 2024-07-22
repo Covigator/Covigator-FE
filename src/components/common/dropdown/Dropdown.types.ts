83626d829e6b66d062fa6e37e9317c9a94585c1a
hyunn522 <tjgus52213@gmail.com>
2024-07-22 17:10:01 +0900

    feat: add types for dropdown component

--- src/components/common/dropdown/Dropdown.types.ts
+++ src/components/common/dropdown/Dropdown.types.ts
@@ -1,1 +1,10 @@
-export type DropdownProps = {};
+export type DropdownProps = {
+  dropdownItems: string[];
+  size?: string;
+};
+
+export const dropdownSize = {
+  sm: 'sm',
+  lg: 'lg',
+} as const;
+export type DropdownSize = (typeof dropdownSize)[keyof typeof dropdownSize];
