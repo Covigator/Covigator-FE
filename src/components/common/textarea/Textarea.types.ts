44bb26fae75b312d84d17c150b85f44c36eb7ec0
hyunn522 <tjgus52213@gmail.com>
2024-07-23 09:14:56 +0900

    feat: add size type

--- src/components/common/textarea/Textarea.types.ts
+++ src/components/common/textarea/Textarea.types.ts
@@ -2,6 +2,13 @@ export type TextareaProps = {
   maxLength: number;
   placeholder: string;
   defaultValue?: string;
+  size: TextareaSize;
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onKeyDown?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
 };
+
+export const textareaSize = {
+  md: 'md',
+  lg: 'lg',
+} as const;
+export type TextareaSize = (typeof textareaSize)[keyof typeof textareaSize];
