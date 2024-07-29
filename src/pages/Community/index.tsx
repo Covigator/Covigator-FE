4025aaf532caf6ba5333c44ca011e9fe5be1489a
hyunn522 <tjgus52213@gmail.com>
2024-07-29 10:42:40 +0900

    feat: add header on community page

--- src/pages/Community/index.tsx
+++ src/pages/Community/index.tsx
@@ -1,5 +1,25 @@
+import Dropdown from '../../components/common/dropdown';
+import Header from '../../components/common/header';
+import { sortDropdownItems } from '../../constants/object';
+
 const index = () => {
-  return <div>Community</div>;
+  return (
+    <div className="w-full pt-[67px] px-[30px]">
+      <header className="relative">
+        <Header
+          title={'커뮤니티'}
+          subtitle={'다른 사용자들과 코스를 공유해보세요'}
+        />
+        <div className="absolute bottom-0 right-0">
+          <Dropdown
+            dropdownItems={sortDropdownItems}
+            size={'sm'}
+            type={'primary'}
+          />
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default index;
