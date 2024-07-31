90e9127e3007e0d6085739eea1463b8a3a9dadbe
HS <hasang2691@naver.com>
2024-07-31 22:46:39 +0900

    Rename: Move Find folder into Login folder

--- src/pages/Login/Find/Password/index.tsx
+++ src/pages/Login/Find/Password/index.tsx
@@ -1,6 +1,6 @@
-import Button from '../../../components/common/button/Button';
-import Input from '../../../components/common/input';
-import { Topbar } from '../../../layouts';
+import Button from '../../../../components/common/button/Button';
+import Input from '../../../../components/common/input';
+import { Topbar } from '../../../../layouts';
 
 const Index = () => {
   return (
