856f46c8645ffee2c548e3d947de329096f5f47a
hyunn522 <tjgus52213@gmail.com>
2024-08-02 01:03:23 +0900

    feat: add dummy data and component for place

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -2,11 +2,33 @@ import { IoMdHeartEmpty } from 'react-icons/io';
 import { useParams } from 'react-router-dom';
 
 import Button from '../../components/common/button';
+import PlaceItem from '../../components/community/PlaceItem';
 import { Topbar } from '../../layouts';
+import { PlaceItemType } from '../../types/community';
 
 const index = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { courseId } = useParams();
+  const dummy: PlaceItemType[] = [
+    {
+      id: 0,
+      type: '카페',
+      name: '스타벅스',
+      desc: '굿굿',
+    },
+    {
+      id: 1,
+      type: '카페',
+      name: '스타벅스',
+      desc: '굿굿',
+    },
+    {
+      id: 2,
+      type: '카페',
+      name: '스타벅스',
+      desc: '굿굿',
+    },
+  ];
 
   return (
     <div className="w-full h-full px-[30px] pt-[62px]">
@@ -25,7 +47,21 @@ const index = () => {
       </header>
       <div className="mt-[17px] w-full h-[221px] bg-bk-40">지도 자리</div>
       <section className="mt-[25px] w-full">
-        <p className="mb-[7px] text-body3 text-bk-80">상세 코스</p>
+        <p className="mb-[7px] text-body3 text-bk-80">코스 장소</p>
+        {dummy.map((d) => {
+          return (
+            <>
+              <PlaceItem
+                key={d.id}
+                type={d.type}
+                name={d.name}
+                desc={d.desc}
+                img={d.img}
+              />
+              <div className="w-full h-[1px] bg-bk-50" />
+            </>
+          );
+        })}
       </section>
     </div>
   );
