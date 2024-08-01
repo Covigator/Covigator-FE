2d082bafed28b76d0c71be86e20f4a362164f73a
HS <hasang2691@naver.com>
2024-08-01 16:25:32 +0900

    Feat: Add blur in Dialog

--- src/components/common/dialog/Dialog.tsx
+++ src/components/common/dialog/Dialog.tsx
@@ -27,41 +27,51 @@ const Dialog: React.FC<DialogProps> = ({
   };
 
   return (
-    <div className="flex flex-col items-center w-[310px] p-[24px_18px_20px_18px] bg-white rounded-[10px]">
-      {showSubtitle ? (
-        <div className="w-[282px] rounded-[10px] flex flex-col gap-y-[8px] mb-[8px]">
-          <h3 className="text-h3 text-center text-bk-100">{title}</h3>
-          <p className="text-body5 text-center text-bk-70">{subtitle}</p>
-        </div>
-      ) : (
-        <h3 className="text-h3 text-center text-bk-100 mb-[8px]">{title}</h3>
-      )}
-      {content && <div className="mb-[18px]">{content}</div>}
-      <div className="flex gap-[16px]">
-        <div className="w-[115px] h-[43px]">
-          <Button
-            size="md"
-            color="sub_300"
-            shape="square"
-            onClick={handleConfirm}
-            className="w-full h-full text-btn2"
-          >
-            {confirmText}
-          </Button>
-        </div>
-        <div className="w-[115px] h-[43px] text-btn2">
-          <Button
-            size="md"
-            color="disabled"
-            shape="square"
-            onClick={handleCancel}
-            className="w-full h-full text-btn2"
-          >
-            {cancelText}
-          </Button>
+    <>
+      <div
+        className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm z-40"
+        onClick={handleCancel}
+      ></div>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50">
+        <div className="flex flex-col items-center w-[310px] p-[24px_18px_20px_18px] bg-white rounded-[10px]">
+          {showSubtitle ? (
+            <div className="w-[282px] rounded-[10px] flex flex-col gap-y-[8px] mb-[8px]">
+              <h3 className="text-h3 text-center text-bk-100">{title}</h3>
+              <p className="text-body5 text-center text-bk-70">{subtitle}</p>
+            </div>
+          ) : (
+            <h3 className="text-h3 text-center text-bk-100 mb-[8px]">
+              {title}
+            </h3>
+          )}
+          {content && <div className="mb-[18px]">{content}</div>}
+          <div className="flex gap-[16px]">
+            <div className="w-[115px] h-[43px]">
+              <Button
+                size="md"
+                color="sub_300"
+                shape="square"
+                onClick={handleConfirm}
+                className="w-full h-full text-btn2"
+              >
+                {confirmText}
+              </Button>
+            </div>
+            <div className="w-[115px] h-[43px] text-btn2">
+              <Button
+                size="md"
+                color="disabled"
+                shape="square"
+                onClick={handleCancel}
+                className="w-full h-full text-btn2"
+              >
+                {cancelText}
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
