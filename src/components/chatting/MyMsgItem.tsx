b0c063c60b073f8058594c7e9ed097c0690ffb1c
hyunn522 <tjgus52213@gmail.com>
2024-08-15 18:43:13 +0900

    feat: seperate type and modify style

--- src/components/chatting/MyMsgItem.tsx
+++ src/components/chatting/MyMsgItem.tsx
@@ -1,15 +1,12 @@
-export type MyMsgItemProps = {
-  text: string;
-  time: string;
-};
+import { MsgItemProps } from '../../types/chatting';
 
-const MyMsgItem = ({ text, time }: MyMsgItemProps) => {
+const MyMsgItem = ({ text, time }: MsgItemProps) => {
   return (
     <div className="inline-flex gap-1 justify-end items-end">
       <span className="text-bk-90 font-pretendard font-light text-[8px]">
         {time}
       </span>
-      <div className="rounded-[20px] rounded-tr-none px-[13px] py-2 bg-bk-30 text-bk-90 text-body5">
+      <div className="rounded-[20px] rounded-tr-none px-[13px] py-2 bg-sub-100 text-wh text-body5">
         {text}
       </div>
     </div>
---
+++ src/types/chatting.ts
@@ -0,0 +1,4 @@
+export type MsgItemProps = {
+  text: string;
+  time: string;
+};
