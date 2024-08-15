d8e242c962fe4e593fbaa060ba61ca22d779ca61
hyunn522 <tjgus52213@gmail.com>
2024-08-15 18:44:02 +0900

    feat: add OtherMsgItem component

---
+++ src/components/chatting/OtherMsgItem.tsx
@@ -0,0 +1,16 @@
+import { MsgItemProps } from '../../types/chatting';
+
+const OtherMsgItem = ({ text, time }: MsgItemProps) => {
+  return (
+    <div className="inline-flex gap-1 justify-start items-end">
+      <div className="rounded-[20px] rounded-tl-none px-[13px] py-2 bg-bk-30 text-bk-90 text-body5">
+        {text}
+      </div>
+      <span className="text-bk-90 font-pretendard font-light text-[8px]">
+        {time}
+      </span>
+    </div>
+  );
+};
+
+export default OtherMsgItem;
