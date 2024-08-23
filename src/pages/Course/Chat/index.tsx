4a948e689631eb710b75f5f70bca1767e25ca39c
hyunn522 <tjgus52213@gmail.com>
2024-08-23 16:23:00 +0900

    refactor: modify the way to transfer data

--- src/pages/Course/Chat/index.tsx
+++ src/pages/Course/Chat/index.tsx
@@ -1,33 +1,33 @@
 import { useState } from 'react';
 import { HiArrowUp } from 'react-icons/hi';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import Input from '../../../components/common/input';
 import { Topbar } from '../../../layouts';
+import { MsgItemType } from '../../../types/chatting';
 
 import clsx from 'clsx';
 
 const Chat = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const courseId = location.state.courseId;
+  const courseName = location.state.courseName;
 
-  const { courseId } = useParams();
   const [inputValue, setInputValue] = useState<string>('');
 
-  const dummy = {
-    courseName: '성수동 데이트',
-    msg: [],
-  };
+  const dummy: MsgItemType[] = [];
 
   return (
     <div className="w-full h-full pt-[60px] px-[20px]">
       <Topbar handleClick={() => navigate(`/course/${courseId}`)}>
         <span className="ml-[14px] text-h4 text-bk-90">
-          {dummy.courseName} 채팅방
+          {courseName} 채팅방
         </span>
       </Topbar>
-      {dummy.msg.length === 0 ? (
-        <div className="mt-[265px] flex justify-center text-body2 text-bk-70">
-          {dummy.courseName} 코스에 대해서
+      {dummy.length === 0 ? (
+        <div className="mt-[265px] flex justify-center text-body2 text-bk-60">
+          {courseName} 코스에 대해서
           <br />
           다른 유저들과 채팅해보세요
         </div>
