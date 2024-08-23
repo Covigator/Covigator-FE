eb64b90a3ba7723dc00cf0f8944d2fbbd96532d1
hyunn522 <tjgus52213@gmail.com>
2024-08-23 16:21:59 +0900

    feat: add type for message item

--- src/types/chatting.ts
+++ src/types/chatting.ts
@@ -2,3 +2,11 @@ export type MsgItemProps = {
   text: string;
   time: string;
 };
+
+export type MsgItemType = {
+  senderId: number;
+  sender: string;
+  senderProfileImg?: string;
+  content: string;
+  createdAt: Date;
+};
