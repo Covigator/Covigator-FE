409c3f06bd83b56bac86bb603fa36c5d8a440311
hyunn522 <tjgus52213@gmail.com>
2024-08-23 18:29:27 +0900

    feat: add work-break property

--- src/components/chatting/OtherMsgItem.tsx
+++ src/components/chatting/OtherMsgItem.tsx
@@ -6,7 +6,7 @@ export const variants = {
   container: 'w-full inline-flex gap-2 justify-start items-start',
   textContainer: 'flex flex-col gap-[7px] mt-[6px]',
   content:
-    'max-w-48 rounded-[20px] px-[13px] py-2 text-body5 whitespace-pre-line',
+    'max-w-48 rounded-[20px] px-[13px] py-2 text-body5 whitespace-pre-line break-all',
   time: 'text-bk-90 font-pretendard font-light text-[8px]',
 };
 
@@ -35,7 +35,7 @@ const OtherMsgItem = ({
           <div
             className={clsx(
               variants.content,
-              'rounded-tl-none bg-bk-30 text-bk-90 ',
+              'rounded-tl-none bg-bk-30 text-bk-90',
             )}
           >
             {text}
