1a4f4abbe6624421bf04d5464c44d64becc7d797
hyunn522 <tjgus52213@gmail.com>
2024-08-27 12:59:19 +0900

    feat: add mycourse page

--- src/pages/Mypage/Mycourse/index.tsx
+++ src/pages/Mypage/Mycourse/index.tsx
@@ -1,5 +1,36 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
+import CourseItem from '../../../components/community/CourseItem';
+import { Topbar } from '../../../layouts';
+import { dummy } from '../../Community';
+
+import { v4 as uuid } from 'uuid';
+
 const Mycourse = () => {
-  return <div>Mycourse</div>;
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  return (
+    <div className="w-full pt-[63px] px-[30px]">
+      <Topbar handleClick={() => navigate('/mypage')} />
+      <p className="text-h3 text-bk-90 mb-[19px]">마이 코스</p>
+      <main className="h-full flex flex-col gap-6 ">
+        {dummy.map((d) => {
+          return (
+            <CourseItem
+              key={uuid()}
+              id={d.id}
+              title={d.title}
+              caption={d.caption}
+              img={d.img}
+              rate={d.rate}
+              isLike={d.isLike}
+            />
+          );
+        })}
+      </main>
+    </div>
+  );
 };
 
 export default Mycourse;
