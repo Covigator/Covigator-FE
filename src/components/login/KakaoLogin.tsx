3c1419d008f7cf88e7de0e70a9265280f14e91e2
HS <hasang2691@naver.com>
2024-09-20 12:30:58 +0900

    Fix: Change kakaologin method from SDK to Rest API

--- src/components/login/KakaoLogin.tsx
+++ src/components/login/KakaoLogin.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { kakaoLogin } from '../../api/auth';
@@ -6,33 +6,8 @@ import kakaoLoginBtn from '../../assets/image/login/kakaoLoginBtn.png';
 import { useAuthStore } from '../../stores/authStore';
 
 const KakaoLogin = () => {
-  const [isKakaoSDKLoaded, setIsKakaoSDKLoaded] = useState(false);
   const navigate = useNavigate();
   const setAuth = useAuthStore((state) => state.setAuth);
-
-  useEffect(() => {
-    // Kakao SDK 로딩 코드
-    const script = document.createElement('script');
-    script.src = 'https://developers.kakao.com/sdk/js/kakao.js';
-    script.async = true;
-    script.onload = () => {
-      if (window.Kakao) {
-        if (!window.Kakao.isInitialized()) {
-          const kakaoJSKey = import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY;
-          if (!kakaoJSKey) {
-            console.error('카카오 자바스크립트 키 에러');
-            return;
-          }
-          window.Kakao.init(kakaoJSKey);
-        }
-        setIsKakaoSDKLoaded(true);
-      }
-    };
-    document.body.appendChild(script);
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
 
   useEffect(() => {
     // URL 파라미터에서 인증 코드 확인
@@ -42,7 +17,7 @@ const KakaoLogin = () => {
     if (authCode) {
       handleKakaoLoginCallback(authCode);
     }
-  }, [navigate, setAuth]);
+  }, []);
 
   const handleKakaoLoginCallback = async (code: string) => {
     try {
@@ -55,29 +30,12 @@ const KakaoLogin = () => {
   };
 
   const handleKakaoLogin = () => {
-    if (!isKakaoSDKLoaded) {
-      console.error('카카오 SDK가 아직 로드되지 않았습니다');
-      return;
-    }
-
-    if (!window.Kakao?.Auth?.authorize) {
-      console.error('카카오 인증 기능을 사용할 수 없습니다');
-      return;
-    }
-
-    const redirectUri = `${import.meta.env.VITE_API_BASE_URL}/accounts/oauth/kakao`;
-
-    window.Kakao.Auth.authorize({
-      redirectUri: redirectUri,
-    });
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${import.meta.env.VITE_KAKAO_JAVASCRIPT_KEY}&redirect_uri=${import.meta.env.VITE_API_BASE_URL}/accounts/oauth/kakao&response_type=code`;
+    window.location.href = KAKAO_AUTH_URL;
   };
 
   return (
-    <button
-      className="w-[280px]"
-      onClick={handleKakaoLogin}
-      disabled={!isKakaoSDKLoaded}
-    >
+    <button className="w-[280px]" onClick={handleKakaoLogin}>
       <img src={kakaoLoginBtn} alt="카카오 로그인" className="w-full" />
     </button>
   );
