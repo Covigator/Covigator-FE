99a5f253eec9a0090a3b3a9705753fe7fb9d9a90
hyunn522 <tjgus52213@gmail.com>
2024-10-12 23:53:02 +0900

    feat: add global setting for SockJS

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  define: { global: {} },
+  define: { global: 'window' },
 });
