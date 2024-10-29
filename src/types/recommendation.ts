23d8dd9e8f326c5e9d63e842f72a69e5068b92ac
HS <hasang2691@naver.com>
2024-10-29 21:42:30 +0900

    feat(recommendation.ts): RecommendResponse interface 생성
    - type/recommendation.ts 생성
    - 해당 파일에 RecommendResponse interface 생성

---
+++ src/types/recommendation.ts
@@ -0,0 +1,12 @@
+interface RecommendResponse {
+  GUNGU: string;
+  LATITUDE: number;
+  LONGITUDE: number;
+  LOTNO_ADDR: string;
+  OPERATION_HOUR: string;
+  PHONE_NUMBER: string;
+  ROAD_NM_ADDR: string;
+  VISIT_AREA_NM: string;
+  VISIT_AREA_TYPE_CD: string;
+  _id: string;
+}
