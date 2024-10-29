8ea19fa1a822f33dac9cd7995965c40730fd2729
hyunn522 <tjgus52213@gmail.com>
2024-10-29 20:24:31 +0900

    feat: add hook for travelStyle on page

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Button from '../../components/common/button/Button';
@@ -7,23 +7,67 @@ import {
   onboardingOptions,
   onboardingQuestions,
 } from '../../constants/onboardingOption';
+import { useTravelStyle } from '../../hooks/api/useMypage';
+import { travelStyleRequest } from '../../types/auth';
 
 import { v4 as uuid } from 'uuid';
 
 const OnboardingPage = () => {
   const navigate = useNavigate();
+  const [requestOptionList, setRequestOptionList] =
+    useState<travelStyleRequest>({
+      gender: '',
+      area_type: '',
+      familiarity: '',
+      activity_type: '',
+      planning_type: '',
+      photo_priority: '',
+      popularity: '',
+    });
+  const fieldOrder = [
+    'gender',
+    'generation',
+    'area_type',
+    'familiarity',
+    'activity_type',
+    'popularity',
+    'planning_type',
+    'photo_priority',
+  ] as const;
+
+  const { mutate, isSuccess } = useTravelStyle(requestOptionList);
 
   const [currentStep, setCurrentStep] = useState(1);
 
   const handleOptionClick = (option: string) => {
-    console.log(`선택한 옵션: ${option}`);
+    const fieldToUpdate = fieldOrder[
+      currentStep - 1
+    ] as keyof travelStyleRequest;
+
+    setRequestOptionList((prev) => ({
+      ...prev,
+      [fieldToUpdate]: option,
+    }));
+
     if (currentStep < onboardingOptions.length) {
       setCurrentStep(currentStep + 1);
-    } else {
-      alert('이제 코스를 짜러 같이 떠나볼까요?');
-      navigate('/');
     }
   };
+
+  useEffect(() => {
+    if (
+      currentStep === onboardingOptions.length &&
+      requestOptionList.photo_priority.length > 0
+    ) {
+      console.log(requestOptionList);
+      console.log(requestOptionList.photo_priority);
+      mutate();
+      if (isSuccess) {
+        alert('이제 코스를 짜러 같이 떠나볼까요?');
+        navigate('/');
+      }
+    }
+  }, [requestOptionList]);
 
   const currentOptions = onboardingOptions[currentStep - 1].options;
   const currentQuestion = onboardingQuestions[currentStep - 1];
@@ -40,12 +84,12 @@ const OnboardingPage = () => {
           currentOptions.map((option) => (
             <div key={uuid()} className={'mb-[34px]'}>
               <Button
-                onClick={() => handleOptionClick(option)}
+                onClick={() => handleOptionClick(option.value)}
                 shape="rounded"
                 size="lg"
                 color="sub"
               >
-                {option}
+                {option.label}
               </Button>
             </div>
           ))}
